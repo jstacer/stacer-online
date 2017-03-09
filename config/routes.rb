b39659bb9068a0767964ca103af99b3edca957ae
Jud Stacer <jud.stacer@gmail.com>
2017-03-08 21:16:43 -0500

    Force SSL

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root 'application#hello'
 
   # Let’s encrypt
-  get '/.well-known/acme-challenge/:id' => 'application#letsencrypt'
+  # get '/.well-known/acme-challenge/:id' => 'application#letsencrypt'
 end
